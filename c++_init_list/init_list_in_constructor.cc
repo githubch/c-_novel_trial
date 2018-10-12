e2451d52549c87addd830328742af6bbabe5b379
dev0 <dev0>
2018-10-12 09:54:42 +0800

    [hcheng] c++ class-variable init not depend on the init list seq

--- c++_init_list/init_list_in_constructor.cc
+++ c++_init_list/init_list_in_constructor.cc
@@ -3,10 +3,12 @@ using namespace std;
 
 class A {
 public:
-	A(int dummy):n2(n1+2),n1(0) { // Init from right to left
+	A(int dummy):n2(n1+2),n1(0) { // It seems like init from right to left, but wrong...
 
 	}
-	A():n2(0),n1(n2+2) { // Init from right to left
+	A():n2(0),n1(n2+2) { // It seems init from right to left, but wrong...
+	}
+	A(int dummy, int dummy1): n1(n2+2), n2(0) { // good case
 	}
 	void Print() {
 		cout << "n1=" << n1 << " ,n2=" << n2 << endl;
@@ -21,6 +23,8 @@ int main(int argc, char** argv) {
 	a.Print();
 	A a1(1);
 	a1.Print();	
+	A a2(1, 1);
+	a2.Print();
 	return 0;
 }
 
