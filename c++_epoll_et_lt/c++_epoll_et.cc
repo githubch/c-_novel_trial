9e1f1a708bd90b88499d5325db45607628e629d0
dev0 <dev0>
2018-11-07 01:42:20 +0800

    [hcheng] 201811070142 test cases for epoll about LT & ET

---
+++ c++_epoll_et_lt/c++_epoll_et.cc
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/epoll.h>
+
+int main()
+{
+    int epfd, nfds;
+    struct epoll_event event, events[5];
+    epfd = epoll_create(1);
+    event.data.fd = STDIN_FILENO;
+    event.events = EPOLLIN | EPOLLET;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, STDIN_FILENO, &event);
+    char input[256];
+    while (1) {
+        nfds = epoll_wait(epfd, events, 5, -1);
+        int i;
+        for (i = 0; i < nfds; ++i) {
+            if (events[i].data.fd == STDIN_FILENO) {
+				read(STDIN_FILENO, input, 1);	
+                printf("hello world\n");
+            }
+        }
+    }
+}
